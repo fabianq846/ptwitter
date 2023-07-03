a0912d8d1b651982b99d87bd8fe72e3cb5b9f729
fabianq846 <fabianq846@gmail.com>
2023-07-03 14:44:00 +0000

    version estable

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+require 'faker'
+
+10.times do |t|
+    Tweet.create(username: Faker::Movies::HarryPotter.character,description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4))
+end 
